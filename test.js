db3fba6270c135192bbb68925aa2845ed8c00ef5
shb003 <123915386+shb003@users.noreply.github.com>
2023-04-20 18:34:57 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 const request = require('supertest');
-const app = require('../index.js');
+const app = require('./index.js');
 
 describe('Index', function() {
   describe('GET /', function() {
