6d4839d46440e12bafee59d191a392417d8b1d4c
shb003 <123915386+shb003@users.noreply.github.com>
2023-04-20 18:30:38 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,17 @@
 const assert = require('assert');
-const app = require('./index.js');
+const request = require('supertest');
+const app = require('../index.js');
 
-describe('index.js', () => {
-  it('should return "Hello, World!" when calling the greet() function', () => {
-    assert.strictEqual(app.greet(), 'Hello, World!');
+describe('Index', function() {
+  describe('GET /', function() {
+    it('should return 200 status code', function(done) {
+      request(app)
+        .get('/')
+        .expect(200)
+        .end(function(err, res) {
+          if (err) done(err);
+          done();
+        });
+    });
   });
 });
